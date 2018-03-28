9d4a480e3fda28f802b1c55c479c16baadca2468
hitheima <hitheima@163.com>
2018-03-28 18:28:16 +0800

    多个测试用例的情况

--- scripts/test_setting.py
+++ scripts/test_setting.py
@@ -5,6 +5,11 @@ from base.base_driver import init_driver
 from page.setting_page import SettingPage
 from base.base_analyze_yml import analyze_yml
 
+def analyze_key(func_name):
+    dict = analyze_yml("setting_data")
+    return dict[func_name]
+
+
 class TestSetting:
 
     def setup(self):
@@ -14,8 +19,17 @@ class TestSetting:
     def teardown(self):
         self.driver.quit()
 
-    @pytest.mark.parametrize("content", analyze_yml("setting_data"))
-    def test_search(self, content):
+    # analyze_keyxxxxxx("file_name" , "key")
+    @pytest.mark.parametrize("content", analyze_key("test_search_english"))
+    def test_search_english(self, content):
+        self.setting_page.click_search()
+        self.setting_page.input_text_view(content)
+        self.setting_page.click_back()
+
+    @pytest.mark.parametrize("content", analyze_key("test_search_chinese"))
+    def test_search_chinese(self, content):
+        self.setting_page.click_search()
+        self.setting_page.click_back()
         self.setting_page.click_search()
         self.setting_page.input_text_view(content)
         self.setting_page.click_back()
